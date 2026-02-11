587f6df10079894660f5a5877004b9421307258a
Lucas Raimundo <lucasraimundo2010@gmail.com>
2026-02-11 13:25:12 -0300

    segundo exercicio de numeros

---
+++ Exercicios Basicos/Numeros/convert.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Desenvolva um programa que leia um número inteiro qualquer e que apresente o número informado com duas casas decimais
+"""
+
+__version__ = '0.1.0'
+__author__ = 'LMR'
+
+num = int(input('Digite um número inteiro: '))
+print(f'O número {num} com duas casas decimais é: {num:.2f}')
