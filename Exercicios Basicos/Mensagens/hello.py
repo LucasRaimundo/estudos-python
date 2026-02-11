1e4de032bfa2b61a16d7259d08298321e5352da0
Lucas Raimundo <lucasraimundo2010@gmail.com>
2026-02-11 18:29:17 -0300

    subindo mais um exercicio de mensagens

--- Exercicios Basicos/Mensagens/hello.py
+++ Exercicios Basicos/Mensagens/hello.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python3
+"""Desenvolva um programa que apresente na tela a seguinte mensagem: Hello World"""
+
+__version__ = '0.1.0'
+__author__ = 'LMR'
+
 print("Hello, World!")
---
+++ Exercicios Basicos/Mensagens/personalizado.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Desenvolva um programa que pergunte o seu nome e, ao teclar Enter, apresente uma saudação personalizada"""
+
+__version__ = '0.1.0'
+__author__ = 'LMR'
+
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}! Seja bem-vindo(a)!")
